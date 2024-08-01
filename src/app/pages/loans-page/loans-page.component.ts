4783c2b66cc53d1c17ef1ae28b7f6a3f96c69b3c
Silali <gibsonsilali@gmail.com>
2024-08-01 12:51:32 +0300

    created loans page

---
+++ src/app/pages/loans-page/loans-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loans-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './loans-page.component.html',
+  styleUrl: './loans-page.component.scss'
+})
+export class LoansPageComponent {
+
+}
