bcd5cc4e1d5746c2dd58e600cb477aa3036748c7
Silali <gibsonsilali@gmail.com>
2024-08-06 08:28:18 +0300

    added data export service for csv

---
+++ src/app/services/common/data-export.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { download, mkConfig, generateCsv } from 'export-to-csv';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataExportService {
+  config = mkConfig({ useKeysAsHeaders: true });
+
+  constructor() {}
+
+  toCSV(data: any) {
+    const csv = generateCsv(this.config)(data);
+    return download(this.config)(csv);
+  }
+
+  toPdf(data: any) {}
+}
